de2237d7d35ad7538737f414fd3dfe65a19215ec
Ziyu Zhang <emacs_dd@yahoo.com>
2016-04-01 08:13:15 +0800

    Adding my existing site.

---
+++ css/bioprint.php
@@ -0,0 +1,199 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html><head><title>CSAIL Biography</title>
+
+
+ <style type="text/css">
+body{font-size:9pt;
+color:red;
+background:white}
+.c1 {color:#00FFFF}
+</style>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="stylesheet" type="text/css" href="bioprint_files/csail.css">
+<link rel="shortcut icon" href="http://www.csail.mit.edu/images/favicon.ico">
+<meta content="text/html; charset=iso-8859-1" http-equiv="Content-Type">
+</head>
+<body bgcolor="#ffffff">
+<table border="0" cellpadding="5" cellspacing="5">
+<tbody><tr>
+	<td colspan="3">
+		<img src="bioprint_files/dotorange.gif" height="3" width="100%"><br>
+	</td>
+</tr>	
+
+<tr>
+	<td>&nbsp;</td>
+	<td>&nbsp;</td>
+	<td colspan="3" class="textpageheader">
+		&nbsp;<br>
+		
+		&nbsp;<br>
+	</td>
+</tr>
+
+<tr>
+	<!-- this column is for the side nav -->
+	<td align="left" valign="top" width="150">
+		<table border="0" cellpadding="0" cellspacing="0">
+	<tbody><tr>
+	<td width="100%"> 
+      <a href="http://www.csail.mit.edu/"><img src="bioprint_files/csaillogosmall.gif" alt="csail logo" border="0" height="76" vspace="5" width="107"></a> 
+
+    </td>
+	</tr>
+	<tr>
+		<td>
+		&nbsp;<br>
+		<img src="bioprint_files/dotorange.gif" alt="orange line" height="2" width="100%">
+		&nbsp;<br>
+		<a href="http://www.csail.mit.edu/index.html" class="pagenav">&nbsp;<img src="bioprint_files/shapeleft.gif" alt="" border="0" height="15" width="16">&nbsp;Home</a><br>
+		
+		<a href="http://www.csail.mit.edu/about/about.html" class="pagenav">&nbsp;<img src="bioprint_files/shapeleft.gif" alt="" border="0" height="15" width="16">&nbsp;About CSAIL</a><br>
+
+		<a href="http://www.csail.mit.edu/about/csailorganization.html" class="pagenav">&nbsp;<img src="bioprint_files/shapeleft.gif" alt="" border="0" height="15" width="16">&nbsp;Lab Organization</a><br>
+		
+		<a href="http://www.csail.mit.edu/directory/directory.php" class="pagenav">&nbsp;<img src="bioprint_files/shapeleft.gif" alt="" border="0" height="15" width="16">&nbsp;Directory</a><br>
+		
+		<a href="http://www.csail.mit.edu/biographies/PI/biolist.php" class="pagenav">&nbsp;<img src="bioprint_files/shapelefthighlight.gif" alt="" border="0" height="15" width="16">&nbsp;Faculty</a><br>
+		
+
+		<a href="http://www.csail.mit.edu/research/activities/activities.php" class="pagenav">&nbsp;<img src="bioprint_files/shapeleft.gif" alt="" border="0" height="15" width="16">&nbsp;Research Activities</a><br>
+		
+		<a href="http://www.csail.mit.edu/events/eventcalendar/calendar.php" class="pagenav">&nbsp;<img src="bioprint_files/shapeleft.gif" alt="" border="0" height="15" width="16">&nbsp;Events</a><br>
+
+		<a href="http://www.csail.mit.edu/events/news/inthenews.html" class="pagenav">&nbsp;<img src="bioprint_files/shapeleft.gif" alt="" border="0" height="15" width="16">&nbsp;News</a><br>
+
+		<a href="http://www.csail.mit.edu/awards/awards.php" class="pagenav">&nbsp;<img src="bioprint_files/shapeleft.gif" alt="" border="0" height="15" width="16">&nbsp;Awards</a><br>
+		
+		<a href="http://www.csail.mit.edu/outreach/outreach.html" class="pagenav">&nbsp;<img src="bioprint_files/shapeleft.gif" alt="" border="0" height="15" width="16">&nbsp;Outreach</a><br>
+
+		<a href="http://www.csail.mit.edu/contact/join.html" class="pagenav">&nbsp;<img src="bioprint_files/shapeleft.gif" alt="" border="0" height="15" width="16">&nbsp;Joining CSAIL</a><br>
+		
+		
+		<a href="http://www.csail.mit.edu/resources/resources.html" class="pagenav">&nbsp;<img src="bioprint_files/shapeleft.gif" alt="" border="0" height="15" width="16">&nbsp;Member Resources</a><br>
+
+		
+		<a href="http://www.csail.mit.edu/join.html" class="pagenav">&nbsp;<img src="bioprint_files/shapeleft.gif" alt="" border="0" height="15" width="16">&nbsp;Contact Us</a><br>
+		</td>
+	</tr>
+	<tr>
+		<td width="100%">
+		<img src="bioprint_files/dotorange.gif" alt="orange line" height="2" width="100%">
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<table>
+			<tbody><tr>
+			<td>
+			&nbsp;<br>
+	<a href="http://publications.csail.mit.edu/"><img src="bioprint_files/publications.gif" alt="button for CSAIL publications web site" border="0" height="28" width="143"></a><br>
+	
+			</td>
+			</tr>
+			</tbody></table>
+			<p>
+			</p><form method="get" action="http://gb-server.mit.edu/search">
+			<input name="q" size="17" maxlength="255" value="" type="text">
+			<input name="btnG" src="bioprint_files/searchbutton.gif" type="image">
+			<input name="site" value="mit" type="hidden">
+			<input name="client" value="mit" type="hidden">
+			<input name="proxystylesheet" value="http://spruce-goose.csail.mit.edu/search/google-csail.xsl" type="hidden">
+			<input name="output" value="xml_no_dtd" type="hidden">
+			<input name="as_dt" value="i" type="hidden">
+			<input name="as_oq" value="site:www.csail.mit.edu site:publications.csail.mit.edu" type="hidden">
+			</form>
+			<p></p>
+		</td>
+	</tr>
+</tbody></table>	
+	</td>
+
+	<!-- this column  provides space between side nav and content -->
+	<td>
+		&nbsp;&nbsp;
+	</td>
+	<td valign="top">
+
+
+
+
+
+
+
+<table border="0" cellpadding="3"><tbody><tr>
+
+<td colspan="2"><span class="textsectionheader2"><p><b> Anant Agarwal</b></p></span>
+<img src="bioprint_files/dotorange.gif" height="2" width="100%"><p></p></td></tr><tr><td width="160">
+<img src="bioprint_files/agarwal.jpg" border="1" height="140" width="140"></td><td class="text" align="left" valign="top">
+<p>Position: Faculty</p>
+<p>Office: 32-G782</p> 
+<p>Phone: 253-1448</p><p>
+</p><p>E-mail: agarwal@csail.mit.edu</p>
+<p>URL <a href="http://cag.csail.mit.edu/%7Eagarwal/">http://cag.csail.mit.edu/~agarwal/</a></p></td></tr><tr align="left" valign="top"><td colspan="2" class="text" valign="top"><p class="textsectionheader3"><br>Biography:<br><img src="bioprint_files/dotorange.gif" height="1" width="100%"></p>Anant
+Agarwal holds a Ph.D. (1987) and an MS in Electrical Engineering from
+Stanford University. He got his bachelor's degree in Electrical
+Engineering from IIT Madras (1982). He is currently a professor of
+Electrical Engineering and Computer Science at MIT, where his teaching
+and research interests include VLSI, computer architecture,
+compilation, and software systems. He led a group that developed the
+MIT Alewife, a scalable shared-memory multiprocessor. He also led the
+VirtualWires project at MIT and was founder of Virtual Machine Works,
+Inc., which took the VirtualWires logic emulation technology to market.
+He currently leads the Raw project in the Computer Science and
+Artificial Intelligence Laboratory. He is also founder of InCert
+Software Corporation, which is applying a software agent technology to
+enterprise applications management.<br><p class="textsectionheader3">Recent and/or Significant Publications:<br><img src="bioprint_files/dotorange.gif" height="1" width="100%"></p>A.
+Agarwal, M. Horowitz, and John Hennessy. "An Analytical Cache Model."
+ACM Transactions on Computer Systems, 7:2, May 1989, pp. 184-215.
+<br>
+<br>A. Agarwal, J. Babb, D. Chaiken, G.D. Souza, K. Johnson, D. Kranz,
+J. Kubiatowicz, B.H. Lim, G. Maa, K. MacKenzie, D. Nussbaum, M. Parkin,
+and D. Yeung. "Sparcle: Today's micro for Tomorrow's Multiprocessor."
+In proceedings of the HOTCHIPS Conference, August 1992, and also in
+IEEE Micro 13:3, June 1993, PP.48-61.
+<br>
+<br>J. Babb, R. Tessier, and A. Agarwal. "Virtual Wires: Overcoming Pin
+Limitations in FPGA-Based Logic Emulators." In proceedings of the First
+IEEE Workshop on FPGAs for Custom Computing Machines (FCCM"93). April
+1993, pp 142-151.
+<br>
+<br>A. Agarwal, R. Bianchini, D. Chaiken, K. Johnson, D. Kranz, J.
+Kubiatowicz, B. H. LIm, K. MacKenzie, and D. Yeung. " The MIT Alewife
+Machine: Architecture and Performance." In proceedings of the 22nd
+Annual Internationsl Symposium on Computer Architecture (ISCA 1995),
+June 1995, pp 2-13.<br></td></tr></tbody></table></td>
+<td>&nbsp;&nbsp;</td>
+ 
+
+</tr>
+
+<tr>
+	<td>&nbsp;</td>
+	<td>&nbsp;</td>
+	<td class="text">
+		<table border="0" cellpadding="5" cellspacing="5" width="100%">
+  <tbody><tr>
+    <td> 
+		 <p class="textvsmall" align="left">
+		 
+		MIT Computer Science and Artificial Intelligence Laboratory (CSAIL)<br>
+        The Stata Center, Building 32 - 32 Vassar Street - Cambridge, MA 02139 - USA 
+        - tel:+1-617-253-5851 - <a href="mailto:webmaster@csail.mit.edu">webmaster@csail.mit.edu</a><br>
+		(Note: On July 1, 2003, 
+        the <a href="http://www.ai.mit.edu/">AI Lab</a> and <a href="http://www.lcs.mit.edu/">LCS</a> 
+        merged to form CSAIL.)</p>
+      </td>
+	    
+  </tr>
+</tbody></table>	</td>
+</tr>
+
+<tr>
+	<td colspan="3">		
+		<img src="bioprint_files/dotorange.gif" alt="" align="top" border="0" height="3" width="100%"><br>
+	</td>
+</tr>	
+</tbody></table>
+</body></html>
---
+++ css/zy.js
@@ -0,0 +1,33 @@
+
+function displayMenu(currentMenu) {
+
+var thisMenu = document.getElementById(currentMenu).style
+
+// The above creates a new function that will expand and contract 
+//certain menus that are specified.
+
+if (thisMenu.display == "block") {
+   thisMenu.display = "none"
+}  
+else {
+   thisMenu.display = "block"
+}
+return false
+}
+
+function displaymenu(currentMenu) {
+
+var thisMenu = document.getElementById(currentMenu).style
+
+// The above creates a new function that will expand and contract 
+//certain menus that are specified.
+
+if (thisMenu.display == "block") {
+   thisMenu.display = "none"
+}  
+else {
+   thisMenu.display = "block"
+}
+return false
+}
+
